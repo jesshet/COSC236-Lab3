d08c3bd96b83aec430f65bdbfd50bbba6fe6dafc
jjsyatco <jjsyatco@yahoo.com>
2024-10-15 14:52:25 -0700

    Update RestaurantApp.java
    Added comments to the changes that I made.

--- src/restaurant/RestaurantApp.java
+++ src/restaurant/RestaurantApp.java
@@ -3,14 +3,16 @@ package restaurant;
 public class RestaurantApp {
     public static void main(String[] args) {
         // Creating customer
-        Customer customer = new Customer("Bob");
+        Customer customer = new Customer("Bob"); // Update customer name to Bob - Jake Yatco
         
         // Creating menu items
-        MenuItem pizza = new MenuItem("Pizza", 9.99);
-        MenuItem burger = new MenuItem("Burger", 6.49);
+        // Change price of ordered menu item by adding 1 dollar - Jake Yatco
+        MenuItem pizza = new MenuItem("Pizza", 9.99); 
+        MenuItem burger = new MenuItem("Burger", 6.49); 
         MenuItem soda = new MenuItem("Soda", 2.99);
         
         // Customer adds items to their order
+        // Update the method name to addToOrder - Jake Yatco
         customer.addToOrder(pizza);  // Collaborating with MenuItem and Order
         customer.addToOrder(burger);
         customer.addToOrder(soda);
